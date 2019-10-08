372b868918cb497d8c27d0398c495587b27aafa1
Fabio Siqueira <fabiohfs85@gmail.com>
2019-10-07 23:30:29 -0300

    Mobile App Initial Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
- 
+const socketio = require('socket.io');
+const http = require('http');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 mongoose.connect('mongodb://omnistack:omnistack@omnistack-shard-00-00-pf1kr.mongodb.net:27017,omnistack-shard-00-01-pf1kr.mongodb.net:27017,omnistack-shard-00-02-pf1kr.mongodb.net:27017/semana09?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+io.on('connection', socket => {
+    const {user_id} = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -16,4 +35,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
